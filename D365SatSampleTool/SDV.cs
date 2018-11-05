5eed5046250562a29da6af310bcd864c798a9e58
Jonas Rapp <jonas@jonasrapp.net>
2018-11-05 21:42:21 +0100

    Adding project and the Simple Data Viewer

---
+++ D365SatSampleTool/SDV.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XrmToolBox.Extensibility;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk;
+using McTools.Xrm.Connection;
+using Microsoft.Xrm.Sdk.Metadata;
+using System.Diagnostics;
+
+namespace D365SatSampleTool
+{
+    public partial class SDV : PluginControlBase
+    {
+        public SDV()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// This event occurs when the connection has been updated in XrmToolBox
+        /// </summary>
+        public override void UpdateConnection(IOrganizationService newService, ConnectionDetail detail, string actionName, object parameter)
+        {
+            base.UpdateConnection(newService, detail, actionName, parameter);
+            gridViews.OrganizationService = detail.ServiceClient;
+            gridData.OrganizationService = detail.ServiceClient;
+            entitiesDropdownControl1.Service = detail.ServiceClient;
+            entitiesDropdownControl1.LoadData();
+        }
+
+        private void LoadViews(EntityMetadata entitymetadata)
+        {
+            if (entitymetadata == null)
+            {
+                gridViews.DataSource = null;
+                return;
+            }
+            WorkAsync(new WorkAsyncInfo
+            {
+                Message = $"Loading views for {entitymetadata.DisplayName.UserLocalizedLabel.Label}",
+                AsyncArgument = entitymetadata,
+                Work = (worker, args) =>
+                {
+                    var meta = args.Argument as EntityMetadata;
+                    var qex = new QueryExpression("savedquery");
+                    qex.ColumnSet.AddColumns("name", "fetchxml");
+                    qex.AddOrder("name", OrderType.Ascending);
+                    qex.Criteria.AddCondition("returnedtypecode", ConditionOperator.Equal, meta.ObjectTypeCode);
+                    qex.Criteria.AddCondition("fetchxml", ConditionOperator.NotNull);
+                    args.Result = Service.RetrieveMultiple(qex);
+                },
+                PostWorkCallBack = (args) =>
+                {
+                    if (args.Error != null)
+                    {
+                        MessageBox.Show(args.Error.Message, "Oh crap", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    if (args.Result is EntityCollection views)
+                    {
+                        gridViews.DataSource = views;
+                    }
+                }
+            });
+        }
+
+        private void LoadData(Entity entity)
+        {
+            if (entity == null)
+            {
+                gridData.DataSource = null;
+                return;
+            }
+            WorkAsync(new WorkAsyncInfo
+            {
+                Message = $"Loading data for {entity["name"]}",
+                AsyncArgument = entity,
+                Work = (worker, args) =>
+                {
+                    var etc = args.Argument as string;
+                    var fex = new FetchExpression(entity["fetchxml"].ToString());
+                    args.Result = Service.RetrieveMultiple(fex);
+                },
+                PostWorkCallBack = (args) =>
+                {
+                    if (args.Error != null)
+                    {
+                        MessageBox.Show(args.Error.Message, "Oh crap", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    if (args.Result is EntityCollection views)
+                    {
+                        gridData.DataSource = views;
+                    }
+                }
+            });
+        }
+
+        private void OpenEntityReference(EntityReference entref)
+        {
+            if (!string.IsNullOrEmpty(entref.LogicalName) && !entref.Id.Equals(Guid.Empty))
+            {
+                var url = ConnectionDetail.WebApplicationUrl;
+                if (string.IsNullOrEmpty(url))
+                {
+                    url = string.Concat(ConnectionDetail.ServerName, "/", ConnectionDetail.Organization);
+                    if (!url.ToLower().StartsWith("http"))
+                    {
+                        url = string.Concat("http://", url);
+                    }
+                }
+                url = string.Concat(url,
+                    url.EndsWith("/") ? "" : "/",
+                    "main.aspx?etn=",
+                    entref.LogicalName,
+                    "&pagetype=entityrecord&id=",
+                    entref.Id.ToString());
+                if (!string.IsNullOrEmpty(url))
+                {
+                    Process.Start(url);
+                }
+            }
+        }
+
+        private void entitiesDropdownControl1_SelectedItemChanged(object sender, Futurez.XrmToolBox.Controls.EntitiesDropdownControl.SelectedItemChangedEventArgs e)
+        {
+            LoadViews(entitiesDropdownControl1.SelectedEntity);
+        }
+
+        private void gridViews_RecordEnter(object sender, Cinteros.Xrm.CRMWinForm.CRMRecordEventArgs e)
+        {
+            LoadData(e.Entity);
+        }
+
+        private void chkFriendly_CheckedChanged(object sender, EventArgs e)
+        {
+            gridData.ShowFriendlyNames = chkFriendly.Checked;
+        }
+
+        private void chkId_CheckedChanged(object sender, EventArgs e)
+        {
+            gridData.ShowIdColumn = chkId.Checked;
+        }
+
+        private void chkLookup_CheckedChanged(object sender, EventArgs e)
+        {
+            gridData.EntityReferenceClickable = chkLookup.Checked;
+        }
+
+        private void gridData_RecordDoubleClick(object sender, Cinteros.Xrm.CRMWinForm.CRMRecordEventArgs e)
+        {
+            if (chkDbl.Checked)
+            {
+                OpenEntityReference(e.Entity.ToEntityReference());
+            }
+        }
+
+        private void gridData_RecordClick(object sender, Cinteros.Xrm.CRMWinForm.CRMRecordEventArgs e)
+        {
+            if (chkLookup.Checked && e.Value is EntityReference entref)
+            {
+                OpenEntityReference(entref);
+            }
+        }
+    }
+}
---
+++ D365SatSampleTool/SDV.designer.cs
@@ -0,0 +1,266 @@
+﻿namespace D365SatSampleTool
+{
+    partial class SDV
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.gridViews = new Cinteros.Xrm.CRMWinForm.CRMGridView();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.entitiesDropdownControl1 = new Futurez.XrmToolBox.Controls.EntitiesDropdownControl();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.gridData = new Cinteros.Xrm.CRMWinForm.CRMGridView();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.chkLookup = new System.Windows.Forms.CheckBox();
+            this.chkDbl = new System.Windows.Forms.CheckBox();
+            this.chkId = new System.Windows.Forms.CheckBox();
+            this.chkFriendly = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViews)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridData)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox4);
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox3);
+            this.splitContainer1.Size = new System.Drawing.Size(831, 533);
+            this.splitContainer1.SplitterDistance = 226;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.gridViews);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 57);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(226, 476);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Views";
+            // 
+            // gridViews
+            // 
+            this.gridViews.AllowUserToAddRows = false;
+            this.gridViews.AllowUserToDeleteRows = false;
+            this.gridViews.AllowUserToOrderColumns = true;
+            this.gridViews.AllowUserToResizeRows = false;
+            this.gridViews.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.gridViews.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridViews.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.name});
+            this.gridViews.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridViews.FilterColumns = "";
+            this.gridViews.Location = new System.Drawing.Point(3, 16);
+            this.gridViews.Name = "gridViews";
+            this.gridViews.ReadOnly = true;
+            this.gridViews.RowHeadersVisible = false;
+            this.gridViews.ShowFriendlyNames = true;
+            this.gridViews.ShowIdColumn = false;
+            this.gridViews.ShowIndexColumn = false;
+            this.gridViews.Size = new System.Drawing.Size(220, 457);
+            this.gridViews.TabIndex = 1;
+            this.gridViews.RecordEnter += new Cinteros.Xrm.CRMWinForm.CRMRecordEventHandler(this.gridViews_RecordEnter);
+            // 
+            // name
+            // 
+            this.name.HeaderText = "View Name";
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            this.name.Width = 86;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.entitiesDropdownControl1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(226, 57);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Entity";
+            // 
+            // entitiesDropdownControl1
+            // 
+            this.entitiesDropdownControl1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.entitiesDropdownControl1.AutoLoadData = false;
+            this.entitiesDropdownControl1.Location = new System.Drawing.Point(6, 22);
+            this.entitiesDropdownControl1.Name = "entitiesDropdownControl1";
+            this.entitiesDropdownControl1.ParentBaseControl = this;
+            this.entitiesDropdownControl1.Service = null;
+            this.entitiesDropdownControl1.Size = new System.Drawing.Size(214, 25);
+            this.entitiesDropdownControl1.TabIndex = 1;
+            this.entitiesDropdownControl1.SelectedItemChanged += new System.EventHandler<Futurez.XrmToolBox.Controls.EntitiesDropdownControl.SelectedItemChangedEventArgs>(this.entitiesDropdownControl1_SelectedItemChanged);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.gridData);
+            this.groupBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox4.Location = new System.Drawing.Point(0, 57);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(601, 476);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Records";
+            // 
+            // gridData
+            // 
+            this.gridData.AllowUserToAddRows = false;
+            this.gridData.AllowUserToDeleteRows = false;
+            this.gridData.AllowUserToOrderColumns = true;
+            this.gridData.AllowUserToResizeRows = false;
+            this.gridData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.gridData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridData.FilterColumns = "";
+            this.gridData.Location = new System.Drawing.Point(3, 16);
+            this.gridData.Name = "gridData";
+            this.gridData.ReadOnly = true;
+            this.gridData.RowHeadersVisible = false;
+            this.gridData.ShowIdColumn = false;
+            this.gridData.ShowIndexColumn = false;
+            this.gridData.Size = new System.Drawing.Size(595, 457);
+            this.gridData.TabIndex = 0;
+            this.gridData.RecordClick += new Cinteros.Xrm.CRMWinForm.CRMRecordEventHandler(this.gridData_RecordClick);
+            this.gridData.RecordDoubleClick += new Cinteros.Xrm.CRMWinForm.CRMRecordEventHandler(this.gridData_RecordDoubleClick);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.chkLookup);
+            this.groupBox3.Controls.Add(this.chkDbl);
+            this.groupBox3.Controls.Add(this.chkId);
+            this.groupBox3.Controls.Add(this.chkFriendly);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox3.Location = new System.Drawing.Point(0, 0);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(601, 57);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Options";
+            // 
+            // chkLookup
+            // 
+            this.chkLookup.AutoSize = true;
+            this.chkLookup.Location = new System.Drawing.Point(285, 30);
+            this.chkLookup.Name = "chkLookup";
+            this.chkLookup.Size = new System.Drawing.Size(96, 17);
+            this.chkLookup.TabIndex = 3;
+            this.chkLookup.Text = "Open Lookups";
+            this.chkLookup.UseVisualStyleBackColor = true;
+            this.chkLookup.CheckedChanged += new System.EventHandler(this.chkLookup_CheckedChanged);
+            // 
+            // chkDbl
+            // 
+            this.chkDbl.AutoSize = true;
+            this.chkDbl.Location = new System.Drawing.Point(155, 30);
+            this.chkDbl.Name = "chkDbl";
+            this.chkDbl.Size = new System.Drawing.Size(124, 17);
+            this.chkDbl.TabIndex = 2;
+            this.chkDbl.Text = "Double-click to open";
+            this.chkDbl.UseVisualStyleBackColor = true;
+            // 
+            // chkId
+            // 
+            this.chkId.AutoSize = true;
+            this.chkId.Location = new System.Drawing.Point(84, 30);
+            this.chkId.Name = "chkId";
+            this.chkId.Size = new System.Drawing.Size(65, 17);
+            this.chkId.TabIndex = 1;
+            this.chkId.Text = "Show Id";
+            this.chkId.UseVisualStyleBackColor = true;
+            this.chkId.CheckedChanged += new System.EventHandler(this.chkId_CheckedChanged);
+            // 
+            // chkFriendly
+            // 
+            this.chkFriendly.AutoSize = true;
+            this.chkFriendly.Location = new System.Drawing.Point(16, 30);
+            this.chkFriendly.Name = "chkFriendly";
+            this.chkFriendly.Size = new System.Drawing.Size(62, 17);
+            this.chkFriendly.TabIndex = 0;
+            this.chkFriendly.Text = "Friendly";
+            this.chkFriendly.UseVisualStyleBackColor = true;
+            this.chkFriendly.CheckedChanged += new System.EventHandler(this.chkFriendly_CheckedChanged);
+            // 
+            // MyPluginControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "MyPluginControl";
+            this.Size = new System.Drawing.Size(831, 533);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridViews)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridData)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private Cinteros.Xrm.CRMWinForm.CRMGridView gridData;
+        private Cinteros.Xrm.CRMWinForm.CRMGridView gridViews;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.CheckBox chkFriendly;
+        private System.Windows.Forms.CheckBox chkId;
+        private Futurez.XrmToolBox.Controls.EntitiesDropdownControl entitiesDropdownControl1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckBox chkDbl;
+        private System.Windows.Forms.CheckBox chkLookup;
+    }
+}
